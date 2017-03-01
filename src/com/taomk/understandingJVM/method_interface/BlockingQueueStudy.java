3cb33f331494004a23008c43fc3ca25ac3d26a5b
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-03-01 22:37:24 +0800

    add blockingqueue study example

---
+++ src/com/taomk/understandingJVM/method_interface/BlockingQueueStudy.java
@@ -0,0 +1,18 @@
+package com.taomk.understandingJVM.method_interface;
+
+/**
+ * BlockingQueue 研究
+ * @author taomk 2017年3月1日 下午10:36:39
+ *
+ */
+public class BlockingQueueStudy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
