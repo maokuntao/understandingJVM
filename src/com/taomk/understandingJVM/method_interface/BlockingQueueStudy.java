cf3d234bffc06c7ef233576ac9f9c20c6f7f9901
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-03-01 22:51:21 +0800

    add blockingqueue study example

--- src/com/taomk/understandingJVM/method_interface/BlockingQueueStudy.java
+++ src/com/taomk/understandingJVM/method_interface/BlockingQueueStudy.java
@@ -1,5 +1,8 @@
 package com.taomk.understandingJVM.method_interface;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * BlockingQueue 研究
  * @author taomk 2017年3月1日 下午10:36:39
@@ -7,12 +10,70 @@ package com.taomk.understandingJVM.method_interface;
  */
 public class BlockingQueueStudy {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
 
+		BlockingQueue<String> queue = new ArrayBlockingQueue<String>(1024);
+		
+		new Thread(new Producer(queue)).start();
+		new Thread(new Consumer(queue)).start();
+		
+		Thread.sleep(3*1000);
 	}
 
+}
+
+
+/**
+ * 生产者
+ * 
+ * @author taomk 2017年3月1日 下午10:49:13
+ *
+ */
+class Producer implements Runnable{
+
+	private BlockingQueue<String> queue;
+	
+	Producer(BlockingQueue<String> queue){
+		this.queue = queue;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			queue.put("A");
+			Thread.sleep(1000);
+			queue.put("B");
+			Thread.sleep(1000);
+			queue.put("C");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+/**
+ * 消费者
+ * 
+ * @author taomk 2017年3月1日 下午10:48:57
+ *
+ */
+class Consumer implements Runnable{
+
+	private BlockingQueue<String> queue;
+	
+	Consumer(BlockingQueue<String> queue){
+		this.queue = queue;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
