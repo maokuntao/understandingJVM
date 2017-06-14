acecf0e79fc18a73f0fea7ec4011249f067dfa1f
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-06-14 11:22:01 +0800

    add lock counter

---
+++ src/com/taomk/understandingJVM/lock/count/Counter.java
@@ -0,0 +1,23 @@
+package com.taomk.understandingJVM.lock.count;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 线程安全的计数器方法
+ * 
+ * @author taomk 2017年6月14日 上午11:17:52
+ *
+ */
+public class Counter {
+
+	private Lock lock = new ReentrantLock();
+	private int count = 0;
+	
+	public int inc(){
+		lock.lock();
+		int newCount = count++;
+		lock.unlock();
+		return newCount;
+	}
+}
