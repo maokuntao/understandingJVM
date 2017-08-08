0a0f2f131f91a402ae5d32a1cd510ff6aac0b7e2
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-08-08 19:47:41 +0800

    add comment

--- src/com/taomk/understandingJVM/nio/FileChannelTest.java
+++ src/com/taomk/understandingJVM/nio/FileChannelTest.java
@@ -3,6 +3,7 @@ package com.taomk.understandingJVM.nio;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 
 /**
@@ -21,16 +22,23 @@ public class FileChannelTest {
 			raf = new RandomAccessFile(filePath, "rw");
 			FileChannel channel = raf.getChannel();
 
-			java.nio.ByteBuffer buffer = ByteBuffer.allocate(1024);
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
 
+			// 将数据读取到buffer
 			int bytesRead = channel.read(buffer);
 			while (bytesRead != -1) {
 				System.out.println("Read " + bytesRead);
+				// 切换buffer模式，从写模式转为读模式
 				buffer.flip();
 				while (buffer.hasRemaining()) {
+					// 读取数据
 					System.out.println((char) buffer.get());
 				}
+				// clear或者compact，
+				// clear会清空整个缓存，
+				// 而compact只会清除已读的数据、未读的数据会移动到缓冲区的起始处
 				buffer.clear();
+				//将数据读取到buffer
 				bytesRead = channel.read(buffer);
 			}
 
