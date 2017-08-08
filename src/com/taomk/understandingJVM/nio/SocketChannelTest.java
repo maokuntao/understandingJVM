5829ef61a24c870ef30762b6822eaab532e792be
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-08-08 23:05:06 +0800

    add socketChannel comment

--- src/com/taomk/understandingJVM/nio/SocketChannelTest.java
+++ src/com/taomk/understandingJVM/nio/SocketChannelTest.java
@@ -7,7 +7,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,12 +24,15 @@ public class SocketChannelTest {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		Thread serverThread = new Thread(() -> server());
+//		Thread serverThread = new Thread(() -> server());
+		Thread serverThread = new Thread(() -> ServerConnect.main(args));
 		serverThread.start();
-		
-		Thread clientThread = new Thread(() -> client());
-		clientThread.start();
-		
+
+		Thread clientThread1 = new Thread(() -> client());
+//		Thread clientThread2 = new Thread(() -> client());
+		clientThread1.start();
+//		clientThread2.start();
+
 		Thread.currentThread().join();
 	}
 
@@ -46,6 +53,7 @@ public class SocketChannelTest {
 				while (true) {// 一直保持活跃状态
 					TimeUnit.SECONDS.sleep(1);
 					String info = "I'm " + i++ + "-th information from client";
+					info += Thread.currentThread().getName();
 					buffer.clear();
 					buffer.put(info.getBytes());
 					buffer.flip();
@@ -103,6 +111,113 @@ public class SocketChannelTest {
 				}
 				if (in != null) {
 					in.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+/**
+ * NIO方式实现的Server端
+ * 
+ * @author taomk 2017年8月8日 下午10:50:35
+ *
+ */
+class ServerConnect {
+	
+	private static final int BUF_SIZE = 1024;
+	private static final int PORT = 8080;
+	private static final int TIMEOUT = 3000;
+
+	public static void main(String[] args) {
+		selector();
+	}
+
+	public static void handleAccept(SelectionKey key) throws IOException {
+		ServerSocketChannel ssChannel = (ServerSocketChannel) key.channel();
+		SocketChannel sc = ssChannel.accept();
+		sc.configureBlocking(false);
+		sc.register(key.selector(), SelectionKey.OP_READ, ByteBuffer.allocateDirect(BUF_SIZE));
+	}
+
+	public static void handleRead(SelectionKey key) throws IOException {
+		SocketChannel sc = (SocketChannel) key.channel();
+		ByteBuffer buf = (ByteBuffer) key.attachment();
+		long bytesRead = sc.read(buf);
+		while (bytesRead > 0) {
+			buf.flip();
+			while (buf.hasRemaining()) {
+				System.out.print((char) buf.get());
+			}
+			System.out.println();
+			buf.clear();
+			bytesRead = sc.read(buf);
+		}
+		if (bytesRead == -1) {
+			sc.close();
+		}
+	}
+
+	public static void handleWrite(SelectionKey key) throws IOException {
+		ByteBuffer buf = (ByteBuffer) key.attachment();
+		buf.flip();
+		SocketChannel sc = (SocketChannel) key.channel();
+		while (buf.hasRemaining()) {
+			sc.write(buf);
+		}
+		buf.compact();
+	}
+
+	public static void selector() {
+		Selector selector = null;
+		ServerSocketChannel ssc = null;
+		try {
+//			打开Serlector
+			selector = Selector.open();
+//			打开ServerSocketChannel
+			ssc = ServerSocketChannel.open();
+//			绑定本机指定端口
+			ssc.socket().bind(new InetSocketAddress(PORT));
+//			非阻塞，channel在与selector配合使用时，必须是非阻塞状态
+			ssc.configureBlocking(false);
+//			注册channel到selector，一旦有client与server接受就绪就会通知到selector
+			ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+			while (true) {//一直保持活跃状态
+				if (selector.select(TIMEOUT) == 0) {
+					System.out.println("==");
+					continue;
+				}
+				Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+				while (iter.hasNext()) {
+					SelectionKey key = iter.next();
+					if (key.isAcceptable()) {
+						handleAccept(key);
+					}
+					if (key.isReadable()) {
+						handleRead(key);
+					}
+					if (key.isWritable() && key.isValid()) {
+						handleWrite(key);
+					}
+					if (key.isConnectable()) {
+						System.out.println("isConnectable = true");
+					}
+					iter.remove();
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (selector != null) {
+					selector.close();
+				}
+				if (ssc != null) {
+					ssc.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
